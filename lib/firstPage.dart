d7751c866ae62ad2f9ae72ffe4f38b62ee357e9c
Ahmad Qudeih <ahmadbfqudeih@gmail.com>
2023-09-09 17:05:54 +0100

    first commit

---
+++ lib/firstPage.dart
@@ -0,0 +1,269 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class First extends StatelessWidget {
+  const First({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color(0xffffc806),
+        elevation: 0,
+        shape: OutlineInputBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10)),
+          borderSide: BorderSide.none,
+        ),
+        leading: Row(
+          children: [Text('data'), Icon(Icons.arrow_back)],
+        ),
+      ),
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 15, left: 16, right: 16),
+              height: 33,
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xffC0C0C0)),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'نتيجة البحث عن وجبلات سريعة',
+                    hintStyle: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.search,
+                      size: 20,
+                      color: Color(0xffffc806),
+                    )),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 18, right: 18, top: 15),
+              child: Row(
+                children: [
+                  Text(
+                    'الوجبات السريعة',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    'شاهد الكل',
+                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            ListView.separated(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 160,
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          spreadRadius: 5,
+                          blurRadius: 5,
+                          color: Color.fromARGB(255, 193, 192, 192))
+                    ],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/Rectangle 18.png',
+                        height: 122,
+                        width: double.infinity,
+                        fit: BoxFit.fill,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Text(
+                          'أبو مازن السوري',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  size: 15,
+                                ),
+                                Text(
+                                  '5.0',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.schedule,
+                                  size: 14,
+                                ),
+                                Text(
+                                  '30-25 دقيقة',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on,
+                                  size: 15,
+                                ),
+                                Text(
+                                  'يبعد 5 كلم',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, index) {
+                return SizedBox(
+                  height: 20,
+                );
+              },
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 18, right: 18, top: 15),
+              child: Row(
+                children: [
+                  Text(
+                    'المطاعم',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    'شاهد الكل',
+                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            GridView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 160,
+                  margin: EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Color(0xffF8F8F8),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/Rectangle 19.png',
+                        height: 122,
+                        width: double.infinity,
+                        fit: BoxFit.fill,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Text(
+                          'شاورما الريم',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  size: 10,
+                                ),
+                                Text(
+                                  '5.0',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.schedule,
+                                  size: 10,
+                                ),
+                                Text(
+                                  '30-25 دقيقة',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on,
+                                  size: 10,
+                                ),
+                                Text(
+                                  'يبعد 5 كلم',
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
